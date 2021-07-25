2492c24f0ddc419ec07cc72c802e31b5544477d6
pravinbangar21 <54072844+pravinbangar21@users.noreply.github.com>
2021-07-26 00:51:31 +0530

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "tf-cicd-state"  #p-pipeline21"
+    bucket = "tf-cicd-state"  
+    #p-pipeline21"
     encrypt = true
     key = "terraform.tfstate"
     region = "us-east-1"
